37c60294859c74a281167c187404d60fdbbe9f99
Mohamed Fazil M <fazilmm860@gmail.com>
2023-08-17 15:46:00 +0530

    edit controller added

--- routers/customerRoute.js
+++ routers/customerRoute.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const { postCustomer, getCustomer } = require('../controllers/custController')
+const { postCustomer, getCustomer, editCustumor } = require('../controllers/custController')
 
 const router = express.Router()
 
 router.post('/sendData', postCustomer);
 router.get('/getdata', getCustomer);
-
+router.put('/edit/:_id', editCustumor)
 module.exports = router
