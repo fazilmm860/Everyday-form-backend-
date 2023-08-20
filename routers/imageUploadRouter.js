d69f44a18e06915230bd726d5f11454ee26be3fb
Mohamed Fazil M <fazilmm860@gmail.com>
2023-08-20 09:07:23 +0530

    Router added uploadImage

---
+++ routers/imageUploadRouter.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { uploadImage } = require('../controllers/ImageUploadController');
+
+const multer = require('multer');
+const storage = multer.memoryStorage(); // Store files in memory for processing
+const upload = multer({ storage: storage });
+
+router.post('/upload', upload.fields([
+    { name: 'aadharFront', maxCount: 1 },
+    { name: 'aadharBack', maxCount: 1 },
+    { name: 'panCard', maxCount: 1 },
+    { name: 'passportSizePhoto', maxCount: 1 }
+]), uploadImage);
+
+
+module.exports = router;
